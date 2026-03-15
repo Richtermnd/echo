ea4592274aedff0cedf90d5f1aa8bffb1eb5cf7b
Richtermnd <m.rustam1305@gmail.com>
2026-03-15 16:09:40 +0000

    simple one connection at time version

--- echo.c
+++ echo.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
+
+void handle_connection(int peer_sock) {
+	char buffer[BUFFER_SIZE];
+	size_t n;
+	for (;;) {
+		n = recv(peer_sock, buffer, BUFFER_SIZE, 0);
+		printf("size: %zu\n", n);
+		printf("msg: %s\n", buffer);
+		if (n == 0) {
+			printf("[INFO] connection closed\n");
+			return;
+		}
+	}
+}
 
 int main() {
-	printf("Hello, World\n");
+	// create socket
+    int s = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	if (s < 0) {
+		fprintf(stderr, "failed to create socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	// bind socket
+	struct sockaddr_in addr = {0};
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(8080);
+	if (bind(s, (struct sockaddr *)(&addr), sizeof(addr))) {
+		fprintf(stderr, "failed to bind socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	// start listening
+	if(listen(s, 0) < 0) {
+		fprintf(stderr, "failed to listen: %s\n", strerror(errno));
+	}
+
+
+	printf("[INFO] echo server started\n");
+	struct sockaddr_in peer_addr = {0};
+	socklen_t peer_addr_size = sizeof(peer_addr);
+	int peer_sock;
+	for (;;) {
+		peer_sock = accept(s, (struct sockaddr *)(&peer_addr), &peer_addr_size); 
+		printf("[INFO] connection accepted\n");
+		handle_connection(peer_sock);
+	}
+	close(s);
 	return 0;
 }
